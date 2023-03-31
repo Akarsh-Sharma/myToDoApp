e4582ac0da045ba9727f104c4e62d0b9b7479859
Akarsh <akarsh311298@gmail.com>
2023-03-31 19:33:09 -0400

    first push

---
+++ webapi/Controllers/ToDoAppController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace webapi.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ToDoAppController : ControllerBase
+    {
+        private static List<string> _todos = new List<string>();
+
+        // GET api/todo
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(_todos);
+        }
+
+        // POST api/todo
+        [HttpPost]
+        public ActionResult Post([FromBody] string todo)
+        {
+            _todos.Add(todo);
+            return Ok();
+        }
+
+        // DELETE api/todo/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0 || id >= _todos.Count)
+            {
+                return BadRequest("Invalid todo ID");
+            }
+            _todos.RemoveAt(id);
+            return Ok();
+        }
+    }
+}
